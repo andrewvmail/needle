de1b51d9918a5bb840b2ad48561440b95140da8d
Momo <andrewvmail@gmail.com>
2021-03-01 21:18:02 -0800

    wip

--- needle/main.swift
+++ needle/main.swift
@@ -1,11 +1,17 @@
-//
-//  main.swift
-//  needle
-//
-//  Created by Momo on 2021-03-01.
-//
-//
-
+import SwiftSyntax
 import Foundation
 
-print("Hello, World!")
+// I want to inject some code after the func decl
+class Needle: SyntaxRewriter {
+  override func visit(_ token: TokenSyntax) -> Syntax {
+    print(token)
+    return Syntax(token)
+  }
+}
+
+let file = "/Users/momo/Desktop/momo.swift"
+let url = URL(fileURLWithPath: file)
+let sourceFile = try SyntaxParser.parse(url)
+let incremented = Needle().visit(sourceFile)
+
+print("hi")
---
+++ test/momo.swift
@@ -0,0 +1,8 @@
+func momo_asking_food(age: Int, name: String) {}
+
+func momo_jumps() {
+  let x = 2
+  let y = 3_000
+}
+
+class Needle: SyntaxRewriter { }
