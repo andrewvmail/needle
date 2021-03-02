789b5591a6fef1b6ff918f36601dfbd87cd98b54
Momo <andrewvmail@gmail.com>
2021-03-01 22:00:11 -0800

    wip: i think i can use this

--- needle/main.swift
+++ needle/main.swift
@@ -3,15 +3,19 @@ import Foundation
 
 // I want to inject some code after the func decl
 class Needle: SyntaxRewriter {
-  override func visit(_ token: TokenSyntax) -> Syntax {
-    print(token)
-    return Syntax(token)
-  }
+    override func visit(_ token: TokenSyntax) -> Syntax {
+        return Syntax(token)
+    }
+    
+    override func visit(_ node: FunctionDeclSyntax) -> DeclSyntax {
+        print("***************FunctionDeclSyntax*********** VVVVV")
+        print(node)
+        print("***************FunctionDeclSyntax*********** ^^^^^^")
+        return DeclSyntax(node)
+    }
 }
 
 let file = "/Users/momo/Desktop/momo.swift"
 let url = URL(fileURLWithPath: file)
 let sourceFile = try SyntaxParser.parse(url)
 let incremented = Needle().visit(sourceFile)
-
-print("hi")
