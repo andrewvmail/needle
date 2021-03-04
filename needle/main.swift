4a96fd37b33b1bb4ac2f459b787ec064cc7d7d6c
Momo <andrewvmail@gmail.com>
2021-03-03 18:25:38 -0800

    nice! this does the trick, prepend into CodeBlockItemListSyntax

--- needle/main.swift
+++ needle/main.swift
@@ -3,19 +3,34 @@ import Foundation
 
 // I want to inject some code after the func decl
 class Needle: SyntaxRewriter {
-    override func visit(_ token: TokenSyntax) -> Syntax {
-        return Syntax(token)
-    }
-    
     override func visit(_ node: FunctionDeclSyntax) -> DeclSyntax {
-        print("***************FunctionDeclSyntax*********** VVVVV")
-        print(node)
-        print("***************FunctionDeclSyntax*********** ^^^^^^")
-        return DeclSyntax(node)
+        var modified = node
+        let newCall = SyntaxFactory.makeFunctionCallExpr(
+            calledExpression: ExprSyntax(SyntaxFactory.makeIdentifierExpr(
+                                            identifier:
+                                                SyntaxFactory.makeIdentifier("Debug.debug")
+                                                .withLeadingTrivia([ .newlines(1), .spaces(2)]),
+                                            declNameArguments: nil)),
+            leftParen: SyntaxFactory.makeLeftParenToken(),
+            argumentList: SyntaxFactory.makeTupleExprElementList([]),
+            rightParen: SyntaxFactory.makeRightParenToken().withTrailingTrivia([.newlines(1), .spaces(0)]),
+            trailingClosure: nil,
+            additionalTrailingClosures: nil)
+        
+        let newStatement = SyntaxFactory.makeCodeBlockItem(
+            item: Syntax(newCall),
+            semicolon: nil,
+            errorTokens: nil)
+        
+        
+        modified.body?.statements = node.body!.statements.prepending(newStatement)
+        return DeclSyntax(modified)
     }
 }
 
-let file = "/Users/momo/Desktop/momo.swift"
+let file = "/Users/momo/projects/needle/test/momo.swift"
 let url = URL(fileURLWithPath: file)
 let sourceFile = try SyntaxParser.parse(url)
-let incremented = Needle().visit(sourceFile)
+let injected = Needle().visit(sourceFile)
+
+print(injected)
--- test/momo.swift
+++ test/momo.swift
@@ -1,6 +1,7 @@
 func momo_asking_food(age: Int, name: String) {}
 
 func momo_jumps() {
+  {}
   let x = 2
   let y = 3_000
 }
